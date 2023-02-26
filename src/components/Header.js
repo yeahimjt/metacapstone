71c572f713fa2296319b6f2bd4d3499801e1714a
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-02-26 10:58:42 -0600

    Wireframe nav, header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,19 @@ import './styles.css'
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="flex bg-[#495E57]">
+      <div className="flex justify-between p-4 max-w-[50%] mx-auto">
+        <div className="flex-column flex-[0.5] p-6">
+          <h1>Little Lemon</h1>
+          <h1>Chicago</h1>
+          <p>Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200 Lorem200</p>
+          <button className="bg-yellow-400 hover:bg-yellow-600 hover:text-white px-4 py-2 rounded">Reserve Now</button>
+        </div>
+        <div className="flex justify-center items-center flex-[0.5]">
+          <div className="p-32 bg-black text-white">Hi</div>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import './styles.css'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Nav = () => {
+  const ref=useRef();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (open && ref.current && !ref.current.contains(e.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [open])
+
+  const handleClick = () => {
+    setOpen(!open)
+    console.log(open)
+  }
   return (
-    <nav>Nav</nav>
+    <nav className="flex w-[100%] p-4 ">
+        <div className="flex justify-between items-center mx-auto p-4 text-black w-[100%] max-w-[1200px]">
+          <div className="">
+              <div className="w-4 h-5">Hi</div>
+          </div>
+          <div className="flex gap-10 ">
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">Home</a>
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">About</a>
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">Menu</a>
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">Reservations</a>
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">Order Line</a>
+              <a className="hover:text-red-900 hover:scale-125 transition-all inline-block" href="" alt="">Login</a>
+          </div>
+          <div className="relative">
+            <AiOutlineMenu className="hover:scale-125 transition-all hover:cursor-pointer" size={20} onClick={handleClick}/>
+            {open &&
+
+                <div className="absolute top-5 right-0 bg-red-500 h-[200px] min-w-[200px] flex flex-col p-4 text-left" ref={ref}>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all " href="" alt="">Home</a>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all" href="" alt="">About</a>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all" href="" alt="">Menu</a>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all" href="" alt="">Reservations</a>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all" href="" alt="">Order Line</a>
+                  <a className="hover:text-red-900 hover:scale-110 transition-all" href="" alt="">Login</a>
+              </div>
+
+          }
+          </div>
+        </div>
+    </nav>
   )
 }
 
